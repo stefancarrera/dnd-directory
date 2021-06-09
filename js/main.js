ea0ec1bde0362274a364c261345bff1eaed5acaf
Stefan Carrera <stefancarrera@gmail.com>
2021-06-08 19:41:17 -0500

    base html & css made with descText array made.

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// var classDesc = [
+//   'A fierce warrior of primitive background who can enter a battle rage.',
+//   'An inspiring magician whose power echoes the music of creation.',
+//   'A priestly champion who wields divine magic in service of a higher power.',
+//   'A priest of the Old Faith, wielding the powers of nature — moonlight and plant growth, fire and lightning — and adopting animal forms.',
+//   'A master of martial combat, skilled with a variety of weapons and armor.',
+//   'A master of martial arts, harnessing the power of the body in pursuit of physical and spiritual perfection.',
+//   'A holy warrior bound to a sacred oath.',
+//   'A warrior who uses martial prowess and nature magic to combat threats on the edges of civilization.',
+//   'A scoundrel who uses stealth and trickery to overcome obstacles and enemies.',
+//   'A spellcaster who draws on inherent magic from a gift or bloodline.',
+//   'A wielder of magic that is derived from a bargain with an extraplanar entity.',
+//   'A scholarly magic-user capable of manipulating the structures of reality.'
+// ];
+
+// var $descP = document.querySelector('.descText');
+// var $equipLi = document.querySelector('#equipLi');
+// var $skillLi = document.querySelector('#skillLi');
+// var $savingThrowLi = document.querySelector('#savingThrowsLi');
